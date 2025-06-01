28169e7e38466c0a4db6d155146f052caca4ba85
Elijah Silang <esilang17279@ucumberlands.edu>
2025-06-01 18:31:42 -0500

    implement quick sort with test cases

---
+++ quick_sort.py
@@ -0,0 +1,24 @@
+def quick_sort(arr):
+    # ends when there is 1 or 0 elements in array
+    if len(arr) <= 1:
+        return arr
+    
+    # pivot selection (element in middle)
+    pivot = arr[len(arr) // 2]
+
+    leftArray = [x for x in arr if x < pivot]
+    middleArray = [x for x in arr if x == pivot]
+    rightArray = [x for x in arr if x > pivot]
+    return quick_sort(leftArray) + middleArray + quick_sort(rightArray)
+
+# Test Case with at least 2 elements
+unsorted_list = [3, 6, 9, 8, 7, 10, 1, 2, 4, 5]
+print("multiple element array (unsorted): " + str(unsorted_list))
+sorted_list = quick_sort(unsorted_list)
+print("multiple element array (sorted):" + str(sorted_list))
+
+# Test case with 1 or 0 elements
+unsorted_list = [4]
+print("single element array (unsorted): " + str(unsorted_list))
+sorted_list = quick_sort(unsorted_list)
+print("single element array (sorted):" + str(sorted_list))
